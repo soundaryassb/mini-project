eac5c77d2511276b553b8a54a2ec47a751af5ab5
soundaryasb <68370011+soundaryassb@users.noreply.github.com>
2021-07-11 18:18:56 +0530

    Update test_cal_operations.c

--- 3_Implementation/test/test_cal_operations.c
+++ 3_Implementation/test/test_cal_operations.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include <calc.h>
+#include <calculator.h>
 #define PROJECT_NAME "Calculator"
 
 void test_add(void);
@@ -7,13 +7,10 @@ void test_sub(void);
 void test_mul(void);
 void test_divi(void);
 void test_mod(void);
-void test_powe(void);
 void test_fact(void);
-void test_xorop(void);
 void test_max(void);
 void test_min(void);
-void test_permutation(void);
-void test_combination(void);
+
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -26,13 +23,9 @@ int main(){
   RUN_TEST(test_mul);
   RUN_TEST(test_divi);
   RUN_TEST(test_mod);
-  RUN_TEST(test_powe);
   RUN_TEST(test_fact);
-  RUN_TEST(test_xorop);
   RUN_TEST(test_max);
   RUN_TEST(test_min);
-  RUN_TEST(test_permutation);
-  RUN_TEST(test_combination);
   return UNITY_END();
 }
 void test_add(void) {
@@ -55,17 +48,10 @@ void test_mod(void){
   TEST_ASSERT_EQUAL(0, (10, 5));
   TEST_ASSERT_EQUAL(3, (3, 4));
 }
-void test_powe(void){
-  TEST_ASSERT_EQUAL(2, (2, 1));
-  TEST_ASSERT_EQUAL(27, (3, 3));
-}
-void test_fact(void){
+
+void test_facto(void){
   TEST_ASSERT_EQUAL(6, (3));
   TEST_ASSERT_EQUAL(120, (5));
-}
-void test_xorop(void){
-  TEST_ASSERT_EQUAL(7, (3, 4));
-  TEST_ASSERT_EQUAL(0, (4, 4));
 }
 void test_max(void){
   TEST_ASSERT_EQUAL(10, (10, 7));
@@ -74,12 +60,4 @@ void test_max(void){
 void test_min(void){
   TEST_ASSERT_EQUAL(7, (10, 7));
   TEST_ASSERT_EQUAL(100, (100, 200));
-}
-void test_permutation(void){
-  TEST_ASSERT_EQUAL(12, (4, 2));
-  TEST_ASSERT_EQUAL(30, (6, 2));
-}
-void test_combination(void){
-  TEST_ASSERT_EQUAL(6, (4, 2));
-  TEST_ASSERT_EQUAL(15, (6, 2));
 }
